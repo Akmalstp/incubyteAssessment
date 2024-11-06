04c102aa4e01b44262aab40cf459dcec754f31bc
Akmalstp <diagovenk@gmail.com>
2024-11-06 20:04:52 +0530

    Added test case to fail when encounter large number of numbers in input string

--- src/main/java/com/akmal/StringCalculator.java
+++ src/main/java/com/akmal/StringCalculator.java
@@ -7,11 +7,15 @@ public class StringCalculator {
 
 	public int add(String numbers) {
 		int numberSum = 0;
-		String[] numberArrayStr = numbers.split(",");
-		for (String numStr : numberArrayStr) {
-			numberSum += Integer.parseInt(numStr);
+		try {
+			String[] numberArrayStr = numbers.split(",");
+			for (String numStr : numberArrayStr) {
+				numberSum += Integer.parseInt(numStr);
+			}
+			System.out.println("Sum is : " + numberSum);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		System.out.println("Sum is : " + numberSum);
 		return numberSum;
 	}
 }
--- src/test/java/com/akmal/StringCalculatorTest.java
+++ src/test/java/com/akmal/StringCalculatorTest.java
@@ -11,4 +11,17 @@ public class StringCalculatorTest {
 		String inputStr = "2,3,4,3,5";
 		Assertions.assertEquals(17, stringCal.add(inputStr));
 	}
+
+	@Test
+	public void testWithLargeNumbers() {
+		StringCalculator stringCal = new StringCalculator();
+		StringBuilder inputStringBuff = new StringBuilder();
+		// already reaching limit for int
+		inputStringBuff.append("2147483647000,");
+		for (int i = 0; i < 500; i++) {
+			inputStringBuff.append(100000 * i).append(",");
+		}
+		inputStringBuff.deleteCharAt(inputStringBuff.length() - 1);
+		Assertions.assertEquals(17, stringCal.add(inputStringBuff.toString()));
+	}
 }
